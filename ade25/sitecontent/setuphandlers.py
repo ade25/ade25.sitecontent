1af7ee9a94d8cf40a3bdd632f8ee5392f370eabb
Christoph Böhner-Figas <boehner@team23.de>
2018-01-19 17:28:34 +0100

    Update installation profile
    Add setup handler and uninstall

---
+++ ade25/sitecontent/setuphandlers.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""Module providing custom setup steps"""
+from plone import api
+
+
+def register_content_types(site):
+    """Run custom add-on package installation code to modify Plone
+       site object and others
+
+    @param site: Plone site
+    """
+    navigation_types = ['ade25.sitecontent.sectionfolder', ]
+    listed_types = api.portal.get_registry_record(
+        name='ade25.base.listed_content_types'
+    )
+    for content_type in navigation_types:
+        if content_type not in listed_types:
+            listed_types.append(content_type)
+    api.portal.set_registry_record(
+        name='ade25.base.listed_content_types',
+        value=listed_types
+    )
+
+
+def setup_various(context):
+    """
+    @param context: Products.GenericSetup.context.DirectoryImportContext instance
+    """
+
+    # We check from our GenericSetup context whether we are running
+    # add-on installation for your package or any other
+    if context.readDataFile('ade25.sitecontent.marker.txt') is None:
+        # Not your add-on
+        return
+
+    portal = context.getSite()
+
+    register_content_types(portal)
