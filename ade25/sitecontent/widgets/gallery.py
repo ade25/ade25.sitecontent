c1773dde64088c353b8092adfa0d96677268fe5d
Christoph Böhner-Figas <boehner@team23.de>
2019-01-18 23:35:01 +0100

    [WIDGET] Add gallery widget scaffolding
    Support basic slider by rendering the existing gallery view
    already available for content pages

--- ade25/sitecontent/widgets/gallery.py
+++ ade25/sitecontent/widgets/gallery.py
@@ -27,9 +27,63 @@ class GalleryWidget(BrowserView):
     def render(self):
         return self.index()
 
+    def has_content(self):
+        return len(self.gallery_items()) > 0
+
     def gallery_items(self):
         # import pdb; pdb.set_trace()
-        return
+        context = aq_inner(self.context)
+        data = context.restrictedTraverse('@@folderListing')(
+            portal_type='Image',
+            sort_on='getObjPositionInParent')
+        return data
+
+    def has_image(self):
+        context = aq_inner(self.context)
+        try:
+            lead_img = context.image
+        except AttributeError:
+            lead_img = None
+        if lead_img is not None:
+            return True
+        return False
+
+    @staticmethod
+    def get_image_data(uuid):
+        tool = getUtility(IResponsiveImageTool)
+        return tool.create(uuid)
+
+
+class GalleryWidgetSlider(BrowserView):
+    """ Gallery widget """
+
+    def __call__(self,
+                 widget_type='gallery-slider',
+                 identifier=None,
+                 data_set=None,
+                 widget_mode='view',
+                 **kw):
+        self.params = {
+            'widget_name': identifier,
+            'widget_type': widget_type,
+            'widget_mode': widget_mode,
+            'widget_data': data_set
+        }
+        return self.render()
+
+    def render(self):
+        return self.index()
+
+    def has_content(self):
+        return len(self.gallery_items()) > 0
+
+    def gallery_items(self):
+        # import pdb; pdb.set_trace()
+        context = aq_inner(self.context)
+        data = context.restrictedTraverse('@@folderListing')(
+            portal_type='Image',
+            sort_on='getObjPositionInParent')
+        return data
 
     def has_image(self):
         context = aq_inner(self.context)
