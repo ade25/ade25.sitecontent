52889954956eb6732036ea898257e1a1f5ac8f36
Christoph Böhner-Figas <boehner@team23.de>
2020-02-10 16:28:45 +0100

    [FEATURE] Cleanup and start control panel import

--- ade25/sitecontent/upgrades.py
+++ ade25/sitecontent/upgrades.py
@@ -8,5 +8,6 @@ logger = logging.getLogger(__name__)
 
 def upgrade_1001(setup):
     setup.runImportStepFromProfile(default_profile, 'typeinfo')
+    setup.runImportStepFromProfile(default_profile, 'controlpanel')
     # Update registry settings
     setup.runImportStepFromProfile(default_profile, 'plone.app.registry')
